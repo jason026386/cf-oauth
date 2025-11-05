eaa68a3caf5dcd46bd80b2c2d8d91900013a13bd
Jason <jason026386@gmail.com>
2025-11-05 11:08:45 +0900

    Update package version to 0.0.9 and add SessionRecord type export to enhance type definitions in OAuth2 handler functions.

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -3,5 +3,6 @@ export { oauth2Logout } from './handlers/logout';
 export { oauth2Me } from './handlers/me';
 export { oauth2Start } from './handlers/start';
 export { oauth2Verify } from './handlers/verify';
+export { SessionRecord } from './types/session';
 export { makeAppleClientSecret } from './utils/pkce';
 //# sourceMappingURL=index.d.ts.map
