d0330d7887fad73fd90bceb60ee47b2d37357fc5
Jason <jason026386@gmail.com>
2025-11-18 10:10:32 +0900

    Update package version to 0.0.12, add OAuth2ProviderConfig and ProviderRegistry exports to enhance type definitions in index files.

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -3,6 +3,7 @@ export { oauth2Logout } from './handlers/logout';
 export { oauth2Me } from './handlers/me';
 export { oauth2Start } from './handlers/start';
 export { oauth2Verify } from './handlers/verify';
+export { OAuth2ProviderConfig, ProviderRegistry } from './types/provider';
 export { SessionRecord } from './types/session';
 export { makeAppleClientSecret } from './utils/pkce';
 //# sourceMappingURL=index.d.ts.map
